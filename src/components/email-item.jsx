2e347a033475c7e88aaf032fe5acbb9bc43823a0
mohibkay <mohibk.in@gmail.com>
2023-10-22 17:11:45 +0530

    feat: add email item

---
+++ src/components/email-item.jsx
@@ -0,0 +1,35 @@
+import { formatDateFromEpoch } from "../lib/utils";
+
+const EmailItem = () => {
+  const name = "Foo Bar";
+  const email = "foo.bar@gmail.com";
+  const subject = "Lorem Ipsum";
+  const shortDescription =
+    "Vestibulum sit amet ipsum aliquet, lacinia nulla malesuada, ullamcorper massa";
+  const date = 1582729505000;
+
+  return (
+    <div className='flex gap-4 border bg-white hover:bg-read-background cursor-pointer px-4 py-3 rounded-lg'>
+      <div className='h-12 w-12 rounded-full bg-highlight text-white grid place-content-center text-xl font-semibold'>
+        F
+      </div>
+
+      <div className='space-y-2'>
+        <header className='space-y-1'>
+          <p>
+            <span className='text-primary-foreground mr-1'>From:</span>
+            <span className='text-primary-foreground font-bold'>{`${name} <${email}>`}</span>
+          </p>
+          <p>
+            <span className='text-primary-foreground mr-1'>Subject:</span>{" "}
+            <span className='text-primary-foreground font-bold'>{subject}</span>
+          </p>
+        </header>
+        <p className='text-primary-foreground '>{shortDescription}</p>
+        <p className='text-primary-foreground '>{formatDateFromEpoch(date)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmailItem;
